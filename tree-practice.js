7906837a0b948ed806c7e919e9be21ac8a1a6ef0
demirose41 <demirose41@gmail.com>
2024-10-23 14:57:40 -0700

    IM OUT

--- tree-practice.js
+++ tree-practice.js
@@ -123,34 +123,86 @@ function getParentNode (rootNode, target) {
       queue.push(node.right);
     }
   }
-  
-  
 
-  
 }
 
 function inOrderPredecessor (rootNode, target) {
-  // Your code here
+  arr = []
+  if(rootNode.left) arr = [...inOrderPredecessor(rootNode.left)]
+  arr.push(rootNode.val)
+  if(rootNode.right) arr = [...arr, ...inOrderPredecessor(rootNode.right)]
+  if(arr[0] === target) return null
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] === target) return arr[i -1] 
+  }
+  return arr;
 }
 
 function deleteNodeBST(rootNode, target) {
   // Do a traversal to find the node. Keep track of the parent
+  let parent = getParentNode(rootNode, target)
+  let foundTarget = undefined
+  let targetChild = undefined
+  // if rootNode is target set it to null*
+  if(rootNode.val === target) {
+    foundTarget = rootNode
+    if(!foundTarget.left && !foundTarget.right){
+      rootNode = null
+      return null
+    }
+  }
+  // if parent is undefined then target does not exists
+  if(parent === undefined) return undefined
 
+  // Find which child is the target
+  if(parent){
+    if(parent.left){
+      if(parent.left.val === target) {
+        // set target
+        foundTarget = parent.left
+        // set which child the target is 
+        targetChild = 'left'
+      } 
+    }if(parent.right){
+      if(parent.right.val === target){
+        foundTarget = parent.right
+        targetChild = 'right'
+      }
+    }
+  }
   // Undefined if the target cannot be found
-
+  if(foundTarget === undefined) return undefined
   // Set target based on parent
 
   // Case 0: Zero children and no parent:
   //   return null
-
+  
   // Case 1: Zero children:
+  if(foundTarget.left === null && foundTarget.right === null){
   //   Set the parent that points to it to null
+    parent[targetChild] = null;
+  }
 
+  if(foundTarget.left === null || foundTarget.right === null){
+    if(foundTarget.left != null) parent[targetChild] = foundTarget.left
+    else parent[targetChild] = foundTarget.right
+  }
   // Case 2: Two children:
-  //  Set the value to its in-order predecessor, then delete the predecessor
-  //  Replace target node with the left most child on its right side, 
-  //  or the right most child on its left side.
-  //  Then delete the child that it was replaced with.
+  if(foundTarget.left  && foundTarget.right ){
+    //  Set the value to its in-order predecessor, then delete the predecessor
+    let currentNode = foundTarget.right
+    while(currentNode.left){
+      currentNode = currentNode.left
+    }
+    
+    //  Replace target node with the left most child on its right side, 
+    foundTarget.val = currentNode.val
+    console.log(currentNode.val)
+    //  or the right most child on its left side.
+    //  Then delete the child that it was replaced with.
+    deleteNodeBST(rootNode, currentNode);
+    
+  }
 
   // Case 3: One child:
   //   Make the parent point to the child
